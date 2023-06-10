e0408a9d4e57864d2afd455856b340c4320e3809
devitadev <75406015+devitadev@users.noreply.github.com>
2023-06-11 04:24:27 +0700

    add map pred n actual p0

--- views/hasil_analisa.py
+++ views/hasil_analisa.py
@@ -35,35 +35,40 @@ def load_view():
     # load style
     with open('./assets/hasil_analisa.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
-    st.header('HASIL ANALISA')
-
-    map_load_state = st.text('Loading Map...')
 
     # load csv data
-    bps_2020 = pd.read_csv('./assets/datas/bps_2020.csv', sep=';')
-    bps_2021 = pd.read_csv('./assets/datas/bps_2021.csv', sep=';')
+    data_complete = pd.read_csv('./assets/datas/ridge_extracted_light_other.csv')
 
-    # read shape data
+    # load shape data
     shp_data = gpd.read_file('./assets/datas/BATAS KABUPATEN KOTA DESEMBER 2019 DUKCAPIL/BATAS KABUPATEN KOTA DESEMBER 2019 DUKCAPIL.shp')
 
+    # section 1
+    st.header('PERSENTASE PENDUDUK MISKIN BERDASARKAN KABUPATEN/KOTA TAHUN 2021')
+
     # merge shape file and bps data
-    data_2020 = pd.DataFrame()
-    data_2020['KAB_KOTA'] = [kab_kota.upper() for kab_kota in bps_2020['Kabupaten/Kota']]
-    data_2020['P0'] = bps_2020['P0']
-    merged_data_2020 = pd.merge(shp_data, data_2020, on='KAB_KOTA')
+    merged_data = pd.merge(shp_data, data_complete, on='KAB_KOTA')
 
     # Create a Folium map object
-    map = folium.Map(location=[-7.242536378184681, 110.21476125405597], scrollWheelZoom = False, zoom_start = 8)
+    map_ypred = folium.Map(location=[-7.242536378184681, 110.21476125405597], scrollWheelZoom = False, zoom_start = 8)
+    map_yactual = folium.Map(location=[-7.242536378184681, 110.21476125405597], scrollWheelZoom = False, zoom_start = 8)
 
     # Add the poverty index as a choropleth map layer
-    choropleth = folium.Choropleth(geo_data = merged_data_2020, data = merged_data_2020, columns = ['KAB_KOTA','P0'],
-        key_on = 'feature.properties.KAB_KOTA', fill_color = 'YlOrRd', fill_opacity = 0.7, line_opacity = 0.2,
-        legend_name='Persentase penduduk miskin (P0)').add_to(map)
-    choropleth.add_to(map)
+    folium.Choropleth(geo_data = merged_data, data = merged_data, columns = ['KAB_KOTA','y pred'],
+            key_on = 'feature.properties.KAB_KOTA', fill_color = 'YlOrRd', fill_opacity = 0.7, line_opacity = 0.2,
+            legend_name='Hasil Prediksi Persentase Penduduk Miskin (P0)').add_to(map_ypred)
+    folium.Choropleth(geo_data = merged_data, data = merged_data, columns = ['KAB_KOTA','y actual'],
+            key_on = 'feature.properties.KAB_KOTA', fill_color = 'YlOrRd', fill_opacity = 0.7, line_opacity = 0.2,
+            legend_name='Persentase Penduduk Miskin (P0)').add_to(map_yactual)
 
     # Display the map using Streamlit
-    folium_static(map)
-    map_load_state.text("Map Loaded")
+    left_co, right_co = st.columns([1, 1])
+    with left_co:
+        map_ypred_state = st.text('Loading Map...')
+        folium_static(map_ypred)
+        map_ypred_state.text("Peta Hasil Prediksi Persentase Penduduk Miskin (P0) Tahun 2021")
+    with right_co:
+        map_yactual_state = st.text('Loading Map...')
+        folium_static(map_yactual)
+        map_yactual_state.text("Peta Persentase Penduduk Miskin (P0) Tahun 2021")
 
     
