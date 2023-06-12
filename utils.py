b673bb49e63a5df05d0df4da73c4ad4ed95f9a08
devitadev <75406015+devitadev@users.noreply.github.com>
2023-06-13 00:02:51 +0700

    make navbar responsive

--- utils.py
+++ utils.py
@@ -19,31 +19,49 @@ def get_current_route():
 
 def navbar_component():
     with open("assets/images/SATELLITIX.png", "rb") as image_file:
-        image_as_base64 = base64.b64encode(image_file.read())
+        image_logo_as_base64 = base64.b64encode(image_file.read())
+
+    with open("assets/images/icon-menu.png", "rb") as image_file:
+        image_menu_as_base64 = base64.b64encode(image_file.read())
 
     navbar_items = ''
     for key, value in NAVBAR_PATHS.items():
-        navbar_items += (f'<a class="navitem" href="/?nav={value}">{key}</a>')
-
+        navbar_items += (f'<a class="navitem navbar-link" href="/?nav={value}">{key}</a>')
+    
     component = rf'''
-        <nav class="container navbar" id="navbar">
-            <img class="nav-logo" src="data:image/png;base64, {image_as_base64.decode("utf-8")}"/>
-            <div> {navbar_items} </div>
+        <nav class="container navbar">
+            <div class="nav-container">
+                <img class="nav-logo" src="data:image/png;base64, {image_logo_as_base64.decode("utf-8")}"/>
+                <img class="navbar-toogle" id="toogle" src="data:image/png;base64, {image_menu_as_base64.decode("utf-8")}"/>
+            </div>
+            <div id="navbar"> {navbar_items} </div>
         </nav>
         '''
     
     st.markdown(component, unsafe_allow_html=True)
-    js = '''
+    
+    nav_js = '''
     <script>
         // navbar elements
         var navigationTabs = window.parent.document.getElementsByClassName("navitem");
         var cleanNavbar = function(navigation_element) {
-            navigation_element.removeAttribute('target')
+            navigation_element.removeAttribute('target');
         }
-        
         for (var i = 0; i < navigationTabs.length; i++) {
             cleanNavbar(navigationTabs[i]);
         }
+
+        var toogle = window.parent.document.getElementById("toogle");
+        var navbar = window.parent.document.getElementById("navbar");
+        toogle.addEventListener("click", () => {
+            if (navbar.style.display == "none") navbar.style.display = "flex";
+            else if (navbar.style.display == "") navbar.style.display = "flex";
+            else navbar.style.display = "none";
+        });
+
+        window.addEventListener("resize", () => {
+            if (toogle.style.display == "none") navbar.style.display = "flex";
+        });
     </script>
     '''
-    html(js)
+    html(nav_js)
