c882b4da1d8a079c653bc4ab0b62e881fc03dc07
devitadev <75406015+devitadev@users.noreply.github.com>
2023-06-16 21:17:25 +0700

    add bar chart tentang data

--- views/tentang_data.py
+++ views/tentang_data.py
@@ -11,11 +11,16 @@ def load_view():
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
     # load csv data
-    df = pd.read_csv('./assets/datas/full_data.csv')
-    kab_kota = [kab_kota_name.lower().title() for kab_kota_name in df['KAB_KOTA']]
-    
-    with st.sidebar:
-        st.header("PARAMETER CITRA SATELIT")
+    df = pd.read_csv('./assets/datas/data persentase penduduk miskin.csv', sep=";")
+    kab_kota = df['Kabupaten/Kota'].copy()
+
+    # section 1
+    st.header('CITRA SATELIT')
+
+    left_co, right_co = st.columns([3, 1])
+
+    with right_co:
+        st.subheader("PARAMETER CITRA SATELIT")
         img_type = st.radio(
             'Pilih tipe citra satelit',
             ('Citra Satelit Siang Hari', 'Citra Satelit Malam Hari')
@@ -26,14 +31,22 @@ def load_view():
         )
         kab_kota_option = st.selectbox('Pilih kabupaten/kota', (kab_kota))
         image_path = './assets/images/' + img_type + ' ' + year + '/' + kab_kota_option + '.png'
-        
     
-    st.header('CITRA SATELIT')
     if img_type == 'Citra Satelit Siang Hari': sumber = "Google Earth Engine, berdasarkan Harmonized Sentinel-2 MSI: MultiSpectral Instrument, Level-2A"
     elif img_type == 'Citra Satelit Malam Hari': sumber = "https://eogdata.mines.edu/nighttime_light/monthly/v10/"
     keterangan = "Citra satelit " + kab_kota_option + " berikut merupakan citra satelit tahun " + str(year) + " yang diperoleh dari " + sumber
+    
+    with left_co:
+        image = Image.open(image_path).resize((1000, 500))
+        st.markdown(keterangan)
+        st.image(image)
+    
+    # section 2
+    st.header('DATA PERSENTASE PENDUDUK MISKIN (P0)')
 
-    image = Image.open(image_path).resize((1000, 500))
-    st.markdown(keterangan)
-    st.image(image)
-        
+    left_co, right_co = st.columns([3, 1])
+    with right_co:
+        plot_year = st.radio("Data Persentase Penduduk Miskin Tahun", ('2020', '2021'))
+    with left_co:
+        st.bar_chart(df, x='Kabupaten/Kota', y=('P0 ' + plot_year))
+    
